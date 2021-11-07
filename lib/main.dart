11bba4ab6e65e37bdb079d2d7da7b827a72d5e97
Wiktor <wijaweq@gmail.com>
2021-11-07 21:13:19 +0100

    Add Settings init to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'game.dart';
+import 'utils/settings.dart';
 
-void main() => runApp(const Minesweeper());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Settings.init();
+  runApp(const Minesweeper());
+}
 
 class Minesweeper extends StatelessWidget {
   const Minesweeper({Key? key}) : super(key: key);
